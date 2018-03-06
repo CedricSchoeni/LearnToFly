91119eab4af3682cdd5d99dfb3b70d6c448ba3d3
adriano.campiotti <adriano.campiotti@gmail.com>
2018-03-06 10:51:27 +0100

    Commented

--- app/src/main/java/mailmaster/cedric/learntofly/Physics/FVector.java
+++ app/src/main/java/mailmaster/cedric/learntofly/Physics/FVector.java
@@ -3,6 +3,7 @@ package mailmaster.cedric.learntofly.Physics;
 
 /**
  * Created by cedric.schoeni on 02.03.2018.
+ * This is a 2 Dimensional Vector class with the methods required to be used by the game.
  */
 
 public class FVector {
@@ -20,26 +21,45 @@ public class FVector {
         this.y = y;
     }
 
+    /**
+     * Adds the x and y of another FVector to this one.
+     * @param fv FVector to use the values of.
+     */
     public void add(FVector fv){
         this.x += fv.x;
         this.y += fv.y;
     }
 
+    /**
+     * Subtracts the x and y of another FVector to this one.
+     * @param fv FVector to use the values of.
+     */
     public void sub(FVector fv){
         this.x -= fv.x;
         this.y -= fv.y;
     }
 
+    /**
+     * Scales the x and y of this FVector by the scale given.
+     * @param scale float which is used to scale this FVector's values.
+     */
     public void mult(float scale){
         this.x *= scale;
         this.y *= scale;
     }
 
+    /**
+     * Scales this FVector's values by another FVector's values (respectively, so x*=fv.x & y*=fv.y)
+     * @param fv FVector to use the values of.
+     */
     public void scale(FVector fv){
         this.x *= fv.x;
         this.y *= fv.y;
     }
 
+    /**
+     * Sets the FVector's x & y to 0.
+     */
     public void reset(){
         this.x = 0;
         this.y = 0;
